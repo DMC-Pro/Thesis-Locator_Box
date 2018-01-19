c3b405af209f27d2020db418526d8232d5df2594
DMC-Pro <dmcpro12@gmail.com>
2018-01-19 14:08:22 +0800

    layout and design adjustments

---
+++ RescueApp/app/src/main/java/com/dmc/androidapp/rescueapp/Connection.java
@@ -0,0 +1,28 @@
+package com.dmc.androidapp.rescueapp;
+
+import java.io.BufferedInputStream;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+/**
+ * Created by DMC Pro on 19/01/2018.
+ */
+
+public class Connection {
+    String data;
+    public String connect() throws MalformedURLException {
+        //URL url = new URL("http://localhost ");
+        //HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
+        //try {
+        //    InputStream in = new BufferedInputStream(urlConnection.getInputStream());
+        //    readStream(in);
+        //} finally {
+        //    urlConnection.disconnect();
+        //}
+        return "nothing";
+    }
+
+
+}
