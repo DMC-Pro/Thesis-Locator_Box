4e0b22970ca1b971ba34d88e1f4d32b9d56bcda2
LittleCammy26 <35369257+LittleCammy26@users.noreply.github.com>
2018-02-02 10:05:16 +0800

    Update index.php

--- Website/index.php
+++ Website/index.php
@@ -6,6 +6,19 @@
 		function drop2() {document.getElementById("dropdown2").classList.toggle("show");}
 		function drop3() {document.getElementById("dropdown3").classList.toggle("show");}
 	</script>
+	<?php
+	function curPageURL() 
+		{$pageURL = 'http';
+		$pageURL .= "://";
+			if ($_SERVER["SERVER_PORT"] != "80") 
+			{$pageURL .= $_SERVER["SERVER_NAME"].":".$_SERVER["SERVER_PORT"].$_SERVER["REQUEST_URI"];}
+			else {$pageURL .= $_SERVER["SERVER_NAME"].$_SERVER["REQUEST_URI"];}
+		return $pageURL;}
+		
+	$result = curPageURL();
+	if ($result=="http://localhost/thesis/index.php")
+		{header("location:http://localhost/thesis/login.php");}
+	?>
 </head>
 <body>
 	<div class="navbar" id="head">
@@ -22,8 +35,23 @@
 	</div>
 	<div id="devices">
 		<p><strong>Active Devices</strong></p>
-		<button class="dropbtn">Device1</button></p>
-		<button class="dropbtn">Device2</button>
+		<?php
+		$dbhost = "localhost";
+		$dbuser = "root";
+		$dbpass = "lifeboxthesis2018";
+		$dbname = "thesis";
+		$count=0;
+		$conn = new mysqli ($dbhost, $dbuser, $dbpass, $dbname);		
+		if ($conn->connect_error) 
+			{die("Connection failed: " . $conn->connect_error);}
+		$query = "SELECT device_status, device_id FROM devices";
+		$result = $conn->query($query);
+		if ($result->num_rows > 0) 
+			{while($row = $result->fetch_assoc()) 
+				{if ($row["device_status"]=="Active")
+					{echo '<button class="dropbtn">'.$row["device_id"]."</button>"."</p>";}}} 
+		else {echo "0 results";}
+		?>
 	</div>
 	<div id="center">
 	</div>
@@ -32,25 +60,43 @@
 		<div class="dropdown">
 			<button onclick="drop1()" class="dropbtn">Respondent1</button>
 			<div id="dropdown1" class="dropdown-content">
-				<a href="#">Location1</a>
-				<a href="#">Location2</a>
-				<a href="#">Location3</a>
+			<?php 
+				$query = "SELECT device_status, device_id FROM devices";
+				$result = $conn->query($query);
+				if ($result->num_rows > 0) 
+					{while($row = $result->fetch_assoc()) 
+						{if ($row["device_status"]=="Active")
+					{		echo '<a href="#">'.$row["device_id"]."</button>"."</a>";}}} 
+				else {echo "0 results";}
+			?>
 			</div>
 		</div></p>
 		<div class="dropdown">
 			<button onclick="drop2()" class="dropbtn">Respondent2</button>
 			<div id="dropdown2" class="dropdown-content">
-				<a href="#">Location1</a>
-				<a href="#">Location2</a>
-				<a href="#">Location3</a>
+				<?php 
+				$query = "SELECT device_status, device_id FROM devices";
+				$result = $conn->query($query);
+				if ($result->num_rows > 0) 
+					{while($row = $result->fetch_assoc()) 
+						{if ($row["device_status"]=="Active")
+					{		echo '<a href="#">'.$row["device_id"]."</button>"."</a>";}}} 
+				else {echo "0 results";}
+			?>
 			</div>
 		</div></p>
 		<div class="dropdown">
 			<button onclick="drop3()" class="dropbtn">Respondent3</button>
 			<div id="dropdown3" class="dropdown-content">
-				<a href="#">Location1</a>
-				<a href="#">Location2</a>
-				<a href="#">Location3</a>
+				<?php 
+				$query = "SELECT device_status, device_id FROM devices";
+				$result = $conn->query($query);
+				if ($result->num_rows > 0) 
+					{while($row = $result->fetch_assoc()) 
+						{if ($row["device_status"]=="Active")
+					{		echo '<a href="#">'.$row["device_id"]."</button>"."</a>";}}} 
+				else {echo "0 results";}
+			?>
 			</div>
 		</div>
 	</div>
