d3ae4744294af20eb653f40192757e0d58e9bde9
LittleCammy26 <35369257+LittleCammy26@users.noreply.github.com>
2018-02-05 14:13:44 +0800

    Update index.php

--- Website/index.php
+++ Website/index.php
@@ -5,7 +5,54 @@
 		function drop1() {document.getElementById("dropdown1").classList.toggle("show");}
 		function drop2() {document.getElementById("dropdown2").classList.toggle("show");}
 		function drop3() {document.getElementById("dropdown3").classList.toggle("show");}
-	</script>
+	
+		function initMap() {
+        var uluru = {lat: -25.363, lng: 131.044};
+        var map = new google.maps.Map(document.getElementById('center'), {
+          zoom: 18,
+          center: uluru
+        });
+        var marker = new google.maps.Marker({
+          position: uluru,
+          map: map
+        });
+		google.maps.event.addDomListener(document.getElementById('dev1'), 'click', function () {
+			var a= document.getElementById('lat1').innerHTML;
+			var b= document.getElementById('lng1').innerHTML;
+			var x= a.substr(0, a.length -1);
+			var y= b.substr(0, b.length -1);
+			if (a.substr(a.length -1, a.length) == "S" && b.substr(b.length -1, b.length) == "W")
+				{var pos = new google.maps.LatLng(-x,-y);}
+			else if (a.substr(a.length -1, a.length) == "N" && b.substr(b.length -1, b.length) == "W")
+				{var pos = new google.maps.LatLng(x,-y);}
+			else if (a.substr(a.length -1, a.length) == "S" && b.substr(b.length -1, b.length) == "E")
+				{var pos = new google.maps.LatLng(-x,y);}
+			else if (a.substr(a.length -1, a.length) == "N" && b.substr(b.length -1, b.length) == "E")
+				{var pos = new google.maps.LatLng(x,y);}
+			map.setCenter(pos);
+			marker.setPosition(pos);
+		});
+		google.maps.event.addDomListener(document.getElementById('dev2'), 'click', function () {
+			var a= document.getElementById('lat2').innerHTML;
+			var b= document.getElementById('lng2').innerHTML;
+			var x= a.substr(0, a.length -1);
+			var y= b.substr(0, b.length -1);
+			if (a.substr(a.length -1, a.length) == "S" && b.substr(b.length -1, b.length) == "W")
+				{var pos = new google.maps.LatLng(-x,-y);}
+			else if (a.substr(a.length -1, a.length) == "N" && b.substr(b.length -1, b.length) == "W")
+				{var pos = new google.maps.LatLng(x,-y);}
+			else if (a.substr(a.length -1, a.length) == "S" && b.substr(b.length -1, b.length) == "E")
+				{var pos = new google.maps.LatLng(-x,y);}
+			else if (a.substr(a.length -1, a.length) == "N" && b.substr(b.length -1, b.length) == "E")
+				{var pos = new google.maps.LatLng(x,y);}
+			map.setCenter(pos);
+			map.setCenter(pos);
+			marker.setPosition(pos);
+		});}
+    </script>
+    <script async defer
+    src="https://maps.googleapis.com/maps/api/js?key=AIzaSyA90ZPwAZuHCBfyQRJYPUNwTm__vSc_dEg&callback=initMap">
+    </script>
 	<?php
 	function curPageURL() 
 		{$pageURL = 'http';
@@ -18,13 +65,17 @@
 	$result = curPageURL();
 	if ($result=="http://localhost/thesis/index.php")
 		{header("location:http://localhost/thesis/login.php");}
+	if ($result=="http://localhost/thesis/res/index.php")
+		{header("location:http://localhost/thesis/index.php");}
+	if ($result=="http://localhost/thesis/interlinks/index.php")
+		{header("location:http://localhost/thesis/index.php");}
 	?>
 </head>
 <body>
 	<div class="navbar" id="head">
 		<img src="http://localhost/thesis/res/logo.png" id="logo" height="50px" width="50px">
 		<a id="map" href="#map">Map</a>
-		<form action="http://localhost/thesis/test1.html"> 
+		<form action="http://localhost/thesis/login.php"> 
 			<button id="out" type="submit">Log out</button>
 		</form>
 		
@@ -40,16 +91,19 @@
 		$dbuser = "root";
 		$dbpass = "lifeboxthesis2018";
 		$dbname = "thesis";
-		$count=0;
+		$count = 1;
 		$conn = new mysqli ($dbhost, $dbuser, $dbpass, $dbname);		
 		if ($conn->connect_error) 
 			{die("Connection failed: " . $conn->connect_error);}
-		$query = "SELECT device_status, device_id FROM devices";
+		$query = "SELECT device_status, device_id, device_latitude, device_longitude FROM devices";
 		$result = $conn->query($query);
 		if ($result->num_rows > 0) 
 			{while($row = $result->fetch_assoc()) 
 				{if ($row["device_status"]=="Active")
-					{echo '<button class="dropbtn">'.$row["device_id"]."</button>"."</p>";}}} 
+					{echo '<button class="dropbtn" id="dev'.$count.'">'.$row["device_id"]."</button>"."</p>";
+					echo '<p class="hidden" id="lat'.$count.'">'.$row["device_latitude"].'</p>';
+					echo '<p class="hidden" id="lng'.$count.'">'.$row["device_longitude"].'</p>';
+					$count=$count+1;}}} 
 		else {echo "0 results";}
 		?>
 	</div>
@@ -58,7 +112,7 @@
 	<div id="respondent">
 		<p><strong>Available Respondents</strong></p>
 		<div class="dropdown">
-			<button onclick="drop1()" class="dropbtn">Respondent1</button>
+			<button onclick="drop1()" class="dropbtn2">Respondent1</button>
 			<div id="dropdown1" class="dropdown-content">
 			<?php 
 				$query = "SELECT device_status, device_id FROM devices";
@@ -72,7 +126,7 @@
 			</div>
 		</div></p>
 		<div class="dropdown">
-			<button onclick="drop2()" class="dropbtn">Respondent2</button>
+			<button onclick="drop2()" class="dropbtn2">Respondent2</button>
 			<div id="dropdown2" class="dropdown-content">
 				<?php 
 				$query = "SELECT device_status, device_id FROM devices";
@@ -86,15 +140,15 @@
 			</div>
 		</div></p>
 		<div class="dropdown">
-			<button onclick="drop3()" class="dropbtn">Respondent3</button>
+			<button onclick="drop3()" class="dropbtn2">Respondent3</button>
 			<div id="dropdown3" class="dropdown-content">
-				<?php 
+				<?php
 				$query = "SELECT device_status, device_id FROM devices";
 				$result = $conn->query($query);
 				if ($result->num_rows > 0) 
 					{while($row = $result->fetch_assoc()) 
 						{if ($row["device_status"]=="Active")
-					{		echo '<a href="#">'.$row["device_id"]."</button>"."</a>";}}} 
+							{echo '<a href="#">'.$row["device_id"]."</a>";}}} 
 				else {echo "0 results";}
 			?>
 			</div>
